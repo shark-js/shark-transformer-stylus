4ba0a45ab5af5e329b358b6934bb99526037473f
Vadim Goncharov <vg@vadimgoncharov.ru>
2014-11-20 19:37:10 +0300

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const chai      = require("chai");
+const expect    = chai.expect;
+
+describe('Initialization', function() {
+	it('should output nothing', function() {
+
+	});
+});
