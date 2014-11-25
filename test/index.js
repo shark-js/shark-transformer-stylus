b155e8be57ba78135760aad10b5d98a9d04f7ed2
Vadim Goncharov <vg@vadimgoncharov.ru>
2014-11-25 16:42:51 +0300

    Create new stylus instance on every destPath in tree

--- test/index.js
+++ test/index.js
@@ -12,33 +12,53 @@ const sprintf   = require('extsprintf').sprintf;
 const cofse     = require('co-fs-extra');
 
 describe('Transofrmation', function() {
-	before(function() {
+	before(function *() {
 		this.logger = Logger({
 			name: 'SharkTransformerStylus'
 		});
+
+		var destA = path.join(__dirname, './fixtures/blocks-a.dest.css');
+		this.destA = destA;
+		this.destExpectA = path.join(__dirname, './fixtures/blocks-a.expect.css');
+		var srcA = path.join(__dirname, './fixtures/blocks-a.styl');
+
+		var destB = path.join(__dirname, './fixtures/blocks-b.dest.css');
+		this.destB = destB;
+		this.destExpectB = path.join(__dirname, './fixtures/blocks-b.expect.css');
+		var srcB = path.join(__dirname, './fixtures/blocks-b.styl');
+
+		var files = {};
+		files[destA] = {
+			files: [srcA, srcB],
+			options: {
+				define: {
+					'$ie8': new transformerStylus.Stylus.nodes.Boolean(false)
+				}
+			}
+		};
+		files[destB] = srcB;
+
+		this.filesTree = yield Tree(files, this.logger);
 	});
 
 	it('should generate and write to file stylus string', function *() {
-		var dest = path.join(__dirname, './fixtures/blocks.css');
-		var src = path.join(__dirname, './fixtures/blocks.styl');
-
-		var files = {};
-		files[dest] = src;
-
-		var filesTree = yield Tree(files, this.logger);
-
 		try {
-			var tree = yield transformerStylus.treeToTree(filesTree, this.logger);
+			var tree = yield transformerStylus.treeToTree(this.filesTree, this.logger, {
+				define: {
+					'$ie8': new transformerStylus.Stylus.nodes.Boolean(true)
+				}
+			});
+
 			yield tree.writeContentToFiles();
-			var contentByStylus = yield cofse.readFile(path.join(__dirname, './fixtures/blocks.css'), {
-				encoding: 'utf8'
-			});
 
-			var contentShouldBe = yield cofse.readFile(path.join(__dirname, './fixtures/blocks.fixture.css'), {
-				encoding: 'utf8'
-			});
+			var contentByPipeA = yield cofse.readFile(this.destA, { encoding: 'utf8' });
+			var contentShouldBeA = yield cofse.readFile(this.destExpectA, { encoding: 'utf8' });
 
-			expect(contentByStylus).equal(contentShouldBe);
+			var contentByPipeB = yield cofse.readFile(this.destA, { encoding: 'utf8' });
+			var contentShouldBeB = yield cofse.readFile(this.destExpectA, { encoding: 'utf8' });
+
+			expect(contentByPipeA).equal(contentShouldBeA);
+			expect(contentByPipeB).equal(contentShouldBeB);
 		}
 		catch (error) {
 			console.error(sprintf('%r', error));
